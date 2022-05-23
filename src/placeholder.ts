a3ba973995d85ed1358b19671ebed9dd8fdf16f9
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-05-23 22:07:21 +0300

    Rewrite library to SVG data-uris
    They are much easier to generate

--- src/placeholder.ts
+++ src/placeholder.ts
@@ -1,68 +1,36 @@
 /**
  * @param {number} width Placeholder width
  * @param {number} height Placeholder height
+ * @param {string?} color Background color (CSS color value)
  * @returns {string} Ready-to-use image src data-uri string.
  */
-export function placeholder(width: number, height: number): string {
+export function placeholder(width: number, height: number, color: string = 'transparent'): string {
   const valid = typeof width === 'number' && typeof height === 'number'
     && width > 0 && width <= 0xFFFF
     && height > 0 && height <= 0xFFFF
     && width === Math.round(width) && height === Math.round(height);
 
   if (!valid) {
-    throw new Error('Width and height have to be integer numbers between 0 and 65536.');
+    throw new Error('Width and height have to be integers greater or equal to 0.');
   }
 
-  const widthBytes = [width % 256, Math.floor(width / 256)];
-  const heightBytes = [height % 256, Math.floor(height / 256)];
+  const image = svg(width, height, color);
 
-  const bytes = [
-    0x47, 0x49, 0x46, // GIF
-    0x38, 0x39, 0x61, // 89a
-
-    // Logical screen descriptor
-    ...widthBytes, // Canvas width
-    ...heightBytes, // Canvas height
-    0b10010000, // Packed field
-    0x00, // Background color index
-    0x00, // Pixel aspect ratio
-
-    // Global color table
-    0x00, 0x00, 0x00,
-    0xFF, 0x00, 0x00,
-
-    // Graphics control extension
-    0x21, // Extension introducer
-    0xF9, // Graphic control label
-    0x04, // Byte size
-    0b00000000, // Packed field
-    0x00, 0x00, // Delay time
-    0x00, // Transparent color index
-    0x00, // Block terminator
-
-    // Image Descriptor
-    0x2C, // Image separator
-    0x00, 0x00, // Image left
-    0x00, 0x00, // Image top
-    ...widthBytes, // Image width
-    ...heightBytes, // Image height
-    0b00000000, // Packed field
-
-    // Image Data
-    0x02, // LZW Minimum Code Size
-    0x00, // Block terminator
-
-    // Trailer
-    0x3B,
-  ];
-
-  return 'data:image/gif;base64,' + base64(bytes);
+  return 'data:image/svg+xml;base64,' + base64(image);
 }
 
-function base64(buffer: number[]): string {
+function svg(width: number, height: number, color: string): string {
+  return (
+    `<svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" xmlns="http://www.w3.org/2000/svg">
+          ${color !== 'transparent' ? `<rect width="${width}" height="${height}" fill="${color}"/>` : ''}
+       </svg>`
+  );
+}
+
+function base64(value: string): string {
   if (typeof btoa === 'function') {
-    return btoa(String.fromCharCode(...buffer));
+    return btoa(value);
   }
 
-  return Buffer.from(buffer).toString('base64');
+  return Buffer.from(value).toString('base64');
 }
